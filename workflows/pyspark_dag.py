5001e774bd7673b9d0cb69025b10904b3df54eb6
BarapatreChetan <barapatrechetan@gmail.com>
2025-03-22 08:26:32 +0530

    co

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "chetan-retailer-project-454413"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-968eb987-bucket"
+COMPOSER_BUCKET = "us-central1-demo-instance-a11a3023-bucket"
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/retailerMysqlToLanding.py"
 PYSPARK_JOB_1 = {
